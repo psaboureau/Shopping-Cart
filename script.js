6d611b39a60c31589b5eb93bff704ac50590bf4f
psaboureau <psaboureau@gmx.fr>
2024-08-26 16:40:48 +0200

    Final touch

--- script.js
+++ script.js
@@ -85,9 +85,8 @@ const products = [
   },
 ];
 
-products.forEach(
-  ({ name, id, price, category }) => {
-    dessertCards.innerHTML += `
+products.forEach(({ name, id, price, category }) => {
+  dessertCards.innerHTML += `
       <div class="dessert-card">
         <h2>${name}</h2>
         <p class="dessert-price">$${price}</p>
@@ -98,8 +97,7 @@ products.forEach(
         </button>
       </div>
     `;
-  }
-);
+});
 
 class ShoppingCart {
   constructor() {
@@ -115,22 +113,25 @@ class ShoppingCart {
 
     const totalCountPerProduct = {};
     this.items.forEach((dessert) => {
-      totalCountPerProduct[dessert.id] = (totalCountPerProduct[dessert.id] || 0) + 1;
-    })
+      totalCountPerProduct[dessert.id] =
+        (totalCountPerProduct[dessert.id] || 0) + 1;
+    });
 
     const currentProductCount = totalCountPerProduct[product.id];
-    const currentProductCountSpan = document.getElementById(`product-count-for-id${id}`);
+    const currentProductCountSpan = document.getElementById(
+      `product-count-for-id${id}`
+    );
 
-    currentProductCount > 1 
-      ? currentProductCountSpan.textContent = `${currentProductCount}x`
-      : productsContainer.innerHTML += `
+    currentProductCount > 1
+      ? (currentProductCountSpan.textContent = `${currentProductCount}x`)
+      : (productsContainer.innerHTML += `
       <div id="dessert${id}" class="product">
         <p>
           <span class="product-count" id="product-count-for-id${id}"></span>${name}
         </p>
         <p>${price}</p>
       </div>
-      `;
+      `);
   }
 
   getCounts() {
@@ -171,20 +172,18 @@ class ShoppingCart {
     cartTotal.textContent = `$${this.total.toFixed(2)}`;
     return this.total;
   }
-};
+}
 
 const cart = new ShoppingCart();
 const addToCartBtns = document.getElementsByClassName("add-to-cart-btn");
 
-[...addToCartBtns].forEach(
-  (btn) => {
-    btn.addEventListener("click", (event) => {
-      cart.addItem(Number(event.target.id), products);
-      totalNumberOfItems.textContent = cart.getCounts();
-      cart.calculateTotal();
-    })
-  }
-);
+[...addToCartBtns].forEach((btn) => {
+  btn.addEventListener("click", (event) => {
+    cart.addItem(Number(event.target.id), products);
+    totalNumberOfItems.textContent = cart.getCounts();
+    cart.calculateTotal();
+  });
+});
 
 cartBtn.addEventListener("click", () => {
   isCartShowing = !isCartShowing;
